18798924ac2d19e03895142b6c36ab2175e9c7c1
Van Nguyen <van.nguyen2@stu.ocsb.ca>
2022-09-14 20:10:56 +0000

    Fixed Linter errors

--- helloWorldUpdated.cpp
+++ helloWorldUpdated.cpp
@@ -1,3 +1,9 @@
+// Copyright (c) 2022 Van Nguyen All rights reserved.
+//
+// Created by: Van Nguyen
+// Date: September 14, 2022
+// Outputs two lines to the console.
+
 #include <iostream>
 
 int main() {
